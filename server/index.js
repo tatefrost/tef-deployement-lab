3cb3c355c18f0b84528a7d5e6e60b181a671d586
Tate Frost <tatefrost96@gmail.com>
2021-12-15 15:29:35 -0700

    fifth commit

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/css', function (req,res) {
+  res.sendFile(path.join(__dirname, "../styles.css"))
+})
+
 const port = process.env.PORT || 4000
 
 app.listen(port, () => (console.log(`Running on port ${port}`)))
