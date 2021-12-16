4632acf4ebf09512410f78fdcb513e6790342e72
Tate Frost <tatefrost96@gmail.com>
2021-12-15 20:09:49 -0700

    16

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ app.get('/index.css', function (req,res) {
   res.sendFile(path.join(__dirname, "../index.css"))
 })
 
+app.get('/js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.js'))
+})
+
 const port = process.env.PORT || 4000
 
 app.listen(port, () => (console.log(`Running on port ${port}`)))
